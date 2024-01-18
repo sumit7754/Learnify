688aa5ce91186c33dc34abb02c045a6f27e201a3
sumit7754 <sumitjain7754@gmail.com>
2024-01-18 12:24:10 +0530

    Added all dependencies

--- server/controllers/payment.contoller.js
+++ server/controllers/payment.contoller.js
@@ -1,6 +1,7 @@
 import User from "../models/user.model";
 import AppError from "../utils/error.util";
 import { razorpay } from '../server.js'
+import crypto from 'crypto'
 
 export const getRazorpayApiKey = async (req,res,next)=>{
        res.status(200).json({
@@ -43,11 +44,90 @@ export const buysciption = async (req,res,next)=>{
 }
 
 export const  verifySubscription = async (req,res,next)=>{
-
+       try {
+        const { id } = req.user;
+        const { razorpay_payment_id, razorpay_subscription_id, razorpay_signature } =
+          req.body;
+      
+        // Finding the user
+        const user = await User.findById(id);
+      
+        // Getting the subscription ID from the user object
+        const subscriptionId = user.subscription.id;
+      
+        // Generating a signature with SHA256 for verification purposes
+        // Here the subscriptionId should be the one which we saved in the DB
+        // razorpay_payment_id is from the frontend and there should be a '|' character between this and subscriptionId
+        // At the end convert it to Hex value
+        const generatedSignature = crypto
+          .createHmac('sha256', process.env.RAZORPAY_SECRET)
+          .update(`${razorpay_payment_id}|${subscriptionId}`)
+          .digest('hex');
+      
+        // Check if generated signature and signature received from the frontend is the same or not
+        if (generatedSignature !== razorpay_signature) {
+          return next(new AppError('Payment not verified, please try again.', 400));
+        }
+      
+        // If they match create payment and store it in the DB
+        await Payment.create({
+          razorpay_payment_id,
+          razorpay_subscription_id,
+          razorpay_signature,
+        });
+      
+        // Update the user subscription status to active (This will be created before this)
+        user.subscription.status = 'active';
+      
+        // Save the user in the DB with any changes
+        await user.save();
+      
+        res.status(200).json({
+          success: true,
+          message: 'Payment verified successfully',
+        });
+       } catch (error) {
+          return next( new AppError(error.message,400));
+       }
 }
 
 export const  cancelSubscriptio = async (req,res,next)=>{
+       try {
+            const { id } = req.user ;
 
+            const user = await User.findById(id);
+    
+            if(!user){
+                return next(new AppError('Unauthorized, please try again!'),500);
+            }
+    
+            if(user.role==='ADMIN'){
+                return next(new AppError('Admin not allowed to purchase'),500);
+            }
+
+             // subscription id
+            const subscriptionId = user.subscription.id;
+
+            // cancel
+            const subscription = await razorpay.subscriptions.cancel(
+                subscriptionId
+            )
+
+            // update
+            user.subscription.status=subscription.status;
+
+           // save
+           await user.save();
+           await payment.save();
+
+           res.status(200).json({
+            success: true,
+            message: 'Subscription canceled successfully',
+          });
+    
+       } catch (error) {
+            return next(new AppError(error.message,400));
+       }
 }
 
 export const  allPayment = async (req,res,next)=>{
