c06dbef990f9877d24be6aff7922ce46739bb435
sumit7754 <sumitjain7754@gmail.com>
2025-01-16 10:08:01 +0530

    Normalize line endings to CRLF

--- server/controllers/miscellaneous.controller.js
+++ server/controllers/miscellaneous.controller.js
@@ -4,10 +4,8 @@ import AppError from '../utils/AppError.js';
 import sendEmail from '../utils/sendEmail.js';
 
 export const contactUs = asyncHandler(async (req, res, next) => {
-  // Destructuring the required data from req.body
   const { name, email, message } = req.body;
 
-  // Checking if values are valid
   if (!name || !email || !message) {
     return next(new AppError('Name, Email, Message are required'));
   }
@@ -15,11 +13,8 @@ export const contactUs = asyncHandler(async (req, res, next) => {
   try {
     const subject = 'Contact Us Form';
     const textMessage = `${name} - ${email} <br /> ${message}`;
-
-    // Await the send email
     await sendEmail(process.env.CONTACT_US_EMAIL, subject, textMessage);
   } catch (error) {
-    console.log(error);
     return next(new AppError(error.message, 400));
   }
 
@@ -29,11 +24,10 @@ export const contactUs = asyncHandler(async (req, res, next) => {
   });
 });
 
-export const userStats = asyncHandler(async (req, res, next) => {
+export const userStats = asyncHandler(async (_req, res) => {
   const allUsersCount = await User.countDocuments();
-
   const subscribedUsersCount = await User.countDocuments({
-    'subscription.status': 'active', // subscription.status means we are going inside an object and we have to put this in quotes
+    'subscription.status': 'active',
   });
 
   res.status(200).json({
--- server/controllers/user.controller.js
+++ server/controllers/user.controller.js
@@ -8,7 +8,7 @@ import sendEmail from '../utils/sendEmail.js';
 
 const cookieOptions = {
   secure: process.env.NODE_ENV === 'production',
-  maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
+  maxAge: 7 * 24 * 60 * 60 * 1000,
   httpOnly: true,
 };
 
